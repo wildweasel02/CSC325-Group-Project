a887eeeef2b31efbf10e56337469ed1ecacb4bae
timarchbold <timarchbold@tims-mbp-2.lan>
2023-02-17 23:40:17 -0500

    Completed executive.java and Staff.java files.

--- src/main/java/com/mycompany/agency/Executive.java
+++ src/main/java/com/mycompany/agency/Executive.java
@@ -10,9 +10,7 @@ public class Executive extends StaffEmployee
     //  Constructor: Sets up this executive with the specified
     //  information.
     //-----------------------------------------------------------------
-    public Executive(String eName, String eAddress, String ePhone,
-            String socSecNumber, double rate)
-    {
+    public Executive(String eName, String eAddress, String ePhone, String socSecNumber, double rate) {
         super(eName, eAddress, ePhone, socSecNumber, rate);
 
         bonus = 0;  // bonus has yet to be awarded
@@ -32,6 +30,7 @@ public class Executive extends StaffEmployee
     //-----------------------------------------------------------------
     public double pay()
     {
-       
+       double payment = super.payRate + bonus;
+       return payment;
     }
 }
